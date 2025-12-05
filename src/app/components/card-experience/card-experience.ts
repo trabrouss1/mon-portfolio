935d54beb76e5aea7fd25f1201171989cc9a4cad
Beh Aboudramane TRAORE <aboudramane97@gmail.com>
2025-12-05 00:28:30 +0000

    feat : Mise en place du composant de l'experience

--- src/app/components/card-experience/card-experience.ts
+++ src/app/components/card-experience/card-experience.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ExperienceService } from '../../services/experience';
+import { ExperienceModel } from '../../models/experience.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-card-experience',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './card-experience.html',
   styleUrl: './card-experience.css',
 })
-export class CardExperience {
+export class CardExperience implements OnInit {
+
+  experiences : ExperienceModel[] = [];
+
+  private experienceService = inject(ExperienceService);
+
+  ngOnInit(): void {
+    this.experiences = this.experienceService.getExperiences();
+  }
 
 }
--- src/app/services/experience.ts
+++ src/app/services/experience.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@angular/core';
+import { ExperienceModel } from '../models/experience.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Experience {
+export class ExperienceService {
   
+  experiences: ExperienceModel[];
+
+  constructor() {
+    this.experiences = [
+      {
+        id: 1,
+        title: 'Développeur Full Stack',
+        company: 'Tech Solutions',
+        startDate: new Date('2022-01-15'),
+        endDate: null,
+        description: 'Développement et maintenance d\'applications web en utilisant Angular et Node.js.'
+      },
+      {
+        id: 2,
+        title: 'Stagiaire Développeur Frontend',
+        company: 'Web Innovators',
+        startDate: new Date('2021-06-01'),
+        endDate: new Date('2021-12-31'),
+        description: 'Participation à la création d\'interfaces utilisateur réactives avec Angular.'
+      }
+    ];
+  }
+
+  getExperiences(): ExperienceModel[] {
+    return this.experiences;
+  }
+
 }
