20b17b26a5aea35f2a4adadc3cc74ea8d68dc8db
Beh Aboudramane TRAORE <aboudramane97@gmail.com>
2025-12-12 22:51:25 +0000

    feat : Reloonking

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './footer.css',
 })
 export class Footer {
-
+  year = new Date().getFullYear();
 }
--- src/app/services/stack.ts
+++ src/app/services/stack.ts
@@ -10,15 +10,16 @@ export class StackService {
 
   constructor() {
     this.stacks = [
-      { nom: 'Angular', iconeUrl: 'icons/angular.svg', couleur: '#DD0031' },
-      { nom: 'TypeScript', iconeUrl: 'icons/typescript.png', couleur: '#3178C6' },
-      { nom: 'Tailwind CSS', iconeUrl: 'icons/tailwindcss.svg', couleur: '#06B6D4' },
+      { nom: 'Angular', iconeUrl: 'icons/angular.svg', couleur: '#DD0031'},
+      { nom: 'TypeScript', iconeUrl: 'icons/typescript.png', couleur: '#3178C6'},
+      { nom: 'Tailwind CSS', iconeUrl: 'icons/tailwindcss.png', couleur: '#06B6D4'},
       { nom: 'Java', iconeUrl: 'icons/java.png', couleur: '#FF9900' },
-      { nom: 'NestJS', iconeUrl: 'icons/nestjs.svg', couleur: '#E0234E' },
-      { nom: 'PostgreSQL', iconeUrl: 'icons/postgresql.png', couleur: '#336791' },
-      { nom: 'Docker', iconeUrl: 'icons/docker.svg', couleur: '#2496ED' },
-      { nom: 'Git', iconeUrl: 'icons/git.png', couleur: '#F05032' },
-      { nom: 'AWS', iconeUrl: 'icons/aws.svg', couleur: '#FF9900' },
+      { nom: 'SpringBoot', iconeUrl: 'icons/spring_boot.png', couleur: '#E0234E'},
+      { nom: 'PostgreSQL', iconeUrl: 'icons/postgresql.png', couleur: '#336791'},
+      { nom: 'Docker', iconeUrl: 'icons/docker.svg', couleur: '#2496ED'},
+      { nom: 'Git', iconeUrl: 'icons/git.png', couleur: '#F05032'},
+      { nom: 'AWS', iconeUrl: 'icons/aws.jpeg', couleur: '#FF9900'},
+      { nom: 'Symfony', iconeUrl: 'icons/symfony.svg', couleur: '#FF9900'},
     ];
   }
 
