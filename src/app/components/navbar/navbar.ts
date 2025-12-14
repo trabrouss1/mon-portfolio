3226d5401d633cd5ceb5786836c6e2114190c43b
Beh Aboudramane TRAORE <aboudramane97@gmail.com>
2025-12-14 10:55:46 +0000

    feat : Mode mobil

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
-
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
+  imports: [CommonModule],
 })
 export class Navbar {
+
+  isMenuOpen: boolean = false;
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 
 }
