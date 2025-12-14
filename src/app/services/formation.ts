a4dcc1ec3f837b211b2caca41fd3255e708714a8
Beh Aboudramane TRAORE <aboudramane97@gmail.com>
2025-12-14 11:50:29 +0000

    Feat : insertion des données correcte formations

--- src/app/services/formation.ts
+++ src/app/services/formation.ts
@@ -16,7 +16,7 @@ export class formationService {
           startDate: new Date('2025-09-01'),
           endDate: new Date('2026-06-30'),
           ecole: 'ESATIC - École Supérieure Africaine des Technologies de l\'Information et de la Communication',
-          description: `Actuellement en cours de Master 1 MBDS à l'ESATIC, je vis la spécialisation dans le domaine de la big data, des systèmes et du cloud computing.`,
+          description: `Actuellement en Master 1 MBDS à l’ESATIC, je souhaite, en complément de mon profil de développeur, approfondir mes connaissances dans les domaines du big data, des systèmes et du cloud computing.`,
           icon: 'diplome',
           color: 'text-orange-600',
         },
@@ -37,7 +37,7 @@ export class formationService {
           startDate: new Date('2018-06-01'),
           endDate: null,
           ecole: 'Collège Les Falaises, Abobo',
-          description: `J'ai obtenu mon Baccalauréat Scientifique (BAC D) avec la mention Assez Bien au Collège Les Falaises à Abobo-Sagbe.`,
+          description: `Baccalauréat Scientifique (BAC D) – Mention Assez Bien, Collège Les Falaises, Abobo-Sagbé.`,
           color: 'text-blue-600',
           icon: 'diplome',
         }
