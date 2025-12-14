4b9beda965b891fbfa49ed3ad4f2475d66bd26fe
Beh Aboudramane TRAORE <aboudramane97@gmail.com>
2025-12-14 11:31:13 +0000

    feat : Mode mobil

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,10 +1,8 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
-  imports: [CommonModule],
 })
 export class Navbar {
 
